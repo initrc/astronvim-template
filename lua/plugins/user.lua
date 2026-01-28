d4cec98ca2df59c5d3e0d3acc229c276d87f8c8e
David Shi <davidsnooker@gmail.com>
2026-01-28 15:53:29 -0800

    Lazy load markdown-preview before calling install()

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -85,6 +85,9 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+      vim.cmd [[Lazy load markdown-preview.nvim]]
+      vim.fn["mkdp#util#install"]()
+    end,
   },
 }
